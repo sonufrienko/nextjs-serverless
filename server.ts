4561cb656ba9cc6fc84ece2d363817459f954f85
Sergey Onufrienko <sergey@onufrienko.com>
2022-12-10 13:26:04 +0100

    add server

---
+++ server.ts
@@ -0,0 +1,19 @@
+import { NextConfig } from 'next';
+import NextServer from 'next/dist/server/next-server';
+import serverless from 'serverless-http';
+// @ts-ignore
+import { config } from './.next/required-server-files.json';
+
+const nextServer = new NextServer({
+  hostname: 'localhost',
+  port: 3000,
+  dir: __dirname,
+  dev: false,
+  conf: {
+    ...(config as NextConfig),
+  },
+});
+
+export const handler = serverless(nextServer.getRequestHandler(), {
+  binary: ['*/*'],
+});
